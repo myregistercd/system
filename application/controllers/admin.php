6f36620dc643e82ceca1d1aca667110ad96b5117
myregistercd <myregistercd@gmail.com>
2014-01-08 17:31:26 +0600

    testing

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -13,7 +13,7 @@ class Admin extends Controller {
 	}
 	
 	function test(){
-			echo "working !!! again !!! the another test is working foafo o";
+			echo "testing for last time ...";
 	}
 
 	function index() {
